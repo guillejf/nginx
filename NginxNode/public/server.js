d90f13ef217a4af0bba5ebf96c7e5ce178276ac5
guillejf <maildeguille@gmail.com>
2022-10-19 11:03:02 -0300

    readme added

--- NginxNode/public/server.js
+++ NginxNode/public/server.js
@@ -13,6 +13,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on PORT ${PORT}`);
 });
 
+//(dentro de la carpeta NginxNode/public donde esta el server.js)
 //pm2 start server.js --name="Server2" --watch -i max -- 8082
 //pm2 start server.js --name="Server1" --watch -- 8081
-//nginx //dentro de la nginx
+
+//(dentro de la carpeta base de to el proyecto donde esta nginx.exe)
+//nginx
